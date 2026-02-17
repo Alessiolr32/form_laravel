cd8331312135b84496b445ce70b953dd9c767d70
Alessio La Rosa <alessiolr32@gmail.com>
2026-02-17 19:33:13 +0100

    update

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,9 @@
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// })->name('home');
+Route::get('/home', function () {
+    return view('welcome');
+})->name('home');
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
